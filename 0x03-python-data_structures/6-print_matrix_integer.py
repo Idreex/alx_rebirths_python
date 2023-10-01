dffe0b7be8743f45f70d2d6b8fccf31705a3fade
idreex <idreesademola07@gmail.com>
2023-10-01 20:58:55 +0100

    matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for item in row:
+            if item == row[-1]:
+                print("{:d}".format(item), end="")
+            else:
+                print("{:d}".format(item), end="")
+            print("")
+
+matrix = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+
+]
+
+
+
+print_matrix_integer(matrix)
