9c8ffd731d670f40aaaf573c13fa85d7d05643c9
idreex <idreesademola07@gmail.com>
2023-10-11 10:01:08 +0100

    intersection of element

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,7 +1,9 @@
 def common_elements(set_1, set_2):
+    return set_1.intersection(set_2)
 
 
 
-    
 set_1 = { "Python", "C", "Javascript" }
 set_2 = { "Bash", "C", "Ruby", "Perl" }
+
+print(common_elements(set_1, set_2))
