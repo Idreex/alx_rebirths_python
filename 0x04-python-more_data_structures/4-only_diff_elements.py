0de42304e966e5e2adf020138da33f9938d2f3d3
idreex <idreesademola07@gmail.com>
2023-10-11 10:57:18 +0100

    diffence_update

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,8 @@
+def only_diff_elements(set_1, set_2):
+    new_set = set_1 - set_2
+    new_set.update(set_2 - set_1)
+    return new_set
+
+set_1 = { "Python", "C", "Javascript" }
+set_2 = { "Bash", "C", "Ruby", "Perl" }
+print(only_diff_elements(set_1, set_2))
