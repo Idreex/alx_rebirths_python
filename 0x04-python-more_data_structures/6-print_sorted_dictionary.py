75479e49391f24d353d0f3dac7e2e9fd04b2b913
idreex <idreesademola07@gmail.com>
2023-10-14 16:46:41 +0100

    sorted_dict

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,12 +1,18 @@
-# def print_sorted_dictionary(a_dictionary):
-    
-#     lst_a_dictionary = list(a_dictionary)
-#     sorted_dictionary = lst_a_dictionary.sort()
-#     print(sorted_dictionary)
+#!/usr/bin/python3
+
+def print_sorted_dictionary(a_dictionary):
+    lst_dict = []
+    for key in a_dictionary.items():
+        lst_dict.append(key)
+        lst_dict.sort()
+    print(dict(lst_dict))
+
+
+
+
 
 
 a_dictionary = { 'language': "C", 'Number': 89, 'track': "Low level", 'ids': [1, 2, 3] }
 
-
-# print_sorted_dictionary(a_dictionary)
+print_sorted_dictionary(a_dictionary)
 
