eabbb3238f1c593577d9d13b9d2bcb528e35bdd5
idreex <idreesademola07@gmail.com>
2023-10-14 12:14:34 +0100

    nb_keys

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -11,6 +11,17 @@ def number_keys(a_dictionary):
     nb_keys = len(a_dictionary)
     print(nb_keys)
     
+# OR 
+nb_keys = 0
+for key in a_dictionary.keys():
+    print(key)
+    nb_keys += 1
+print(nb_keys)
 
+# for Values:
+nb_values = 0
+for value in a_dictionary.values():
+    nb_values += 1
+print(nb_values)
 number_keys(a_dictionary)
     
---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,12 @@
+# def print_sorted_dictionary(a_dictionary):
+    
+#     lst_a_dictionary = list(a_dictionary)
+#     sorted_dictionary = lst_a_dictionary.sort()
+#     print(sorted_dictionary)
+
+
+a_dictionary = { 'language': "C", 'Number': 89, 'track': "Low level", 'ids': [1, 2, 3] }
+
+
+# print_sorted_dictionary(a_dictionary)
+
