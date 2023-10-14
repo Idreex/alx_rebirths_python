664c1c033ae5eae45f7df72bfc5acd223b1ffe59
idreex <idreesademola07@gmail.com>
2023-10-14 08:32:36 +0100

    nb_keys

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -2,16 +2,15 @@
 
 a_dictionary = { 'language': "C", 'number': 13, 'track': "Low level" }
 def number_keys(a_dictionary):
-    i = 0
-    for k in a_dictionary.items():
-        return k
-    i += 1
-    print(i)
-
-
     
-    a_dictionary = { 'language': "C", 'number': 13, 'track': "Low level" }
-    # nb_keys = number_keys(a_dictionary)
-    # print("Number of keys: {:d}".format(nb_keys))
+    lst_dict = list(a_dictionary)
+    print(len(lst_dict))
+
+# OR
+
+    nb_keys = len(a_dictionary)
+    print(nb_keys)
+    
+
 number_keys(a_dictionary)
     
