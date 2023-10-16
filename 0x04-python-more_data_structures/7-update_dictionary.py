b1dcae8e336f312058fb45b7514f5fee8ff0121d
idreex <idreesademola07@gmail.com>
2023-10-16 22:42:55 +0100

    update

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,11 +1,29 @@
 #!/usr/bin/python3
 
 def update_dictionary(a_dictionary, key, value):
-    
-    pass
-    
-    
-    
-a_dictionary = { 'language': "C", 'number': 89, 'track': "Low level" }
+    a_dictionary.update('language':'Python')
+ 
+    a_dictionary = { 'language': "C", 'number': 89, 'track': "Low level" }
 
-new_dict = update_dictionary(a_dictionary, 'language', "Python")
+
+
+
+def sum(items):
+    head, *tail = items
+    return head + sum(tail) if tail else head
+
+items = [1,3,5,9]
+print(sum(items))
+
+
+
+def sum(*args):
+    total = 0
+    for arg in args:
+        total += arg
+    
+    return total
+
+#args = [1,2,3,4]
+
+print(sum(9,2,3,4))
