d466228495388c77220665a70227b045c3da38e3
idreex <idreesademola07@gmail.com>
2023-10-26 23:01:20 +0100

    second-method

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,18 +4,37 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for item in row:
             if item == row[-1]:
-                print(f"{item}",end="")
+                print("{:d}".format(item), end="")
             else:
-                print("{item}", end=" ")
-            print("")
+                print("{:d}".format(item), end=" ")
+            print()
+            
 
 matrix = [
     [1,2,3],
     [4,5,6],
     [7,8,9]
-
 ]
 
+''' if we want it to print inside list that could be like this'''
+def matrix_int_lists(matrix=[[]]):
+    for row in matrix:
+        new_matrix = []
+        for element in row:
+            new_matrix.append(element)
+        print(new_matrix)
 
+print('\n')
+print('\n')
+
+''' if we want it to print inside list that could be like this'''
+def matrix_int_listd(matrix=[[]]):
+    for row in matrix:
+        new_matrix = []
+        for element in row:
+            new_matrix.append(element*element)
+        print(new_matrix)
 
 print_matrix_integer(matrix)
+matrix_int_lists(matrix)
+matrix_int_listd(matrix)
