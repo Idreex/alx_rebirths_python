8ade9a20d84defdd7ca321b6e398bc14d2a6939e
Idreex <idreesademola07@gmail.com>
2023-11-08 22:39:39 +0100

    safe_print

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    try:
+        for i in range(x):
+            print(my_list[i], end="")
+    except:
+        
+
+
+
+
+
+
+my_list = [1, 2, 3, 4, 5]
+safe_print_list()
