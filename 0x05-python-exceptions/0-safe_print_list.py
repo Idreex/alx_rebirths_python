0d5f529ef3828fe565a9c65f11a018f66a3bec44
Idreex <idreesademola07@gmail.com>
2023-11-13 00:09:20 +0100

    tested

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,10 +4,14 @@ def safe_print_list(my_list=[], x=0):
     try:
         for i in range(x):
             print(my_list[i], end="")
+        print()
+        print(f'number of items to print is: {x}')
     except IndexError as e:
         print('Index not exist')
+    
+    
         
 
 
 my_list = [1, 2, 3, 4, 5]
-safe_print_list()
+safe_print_list(my_list,2)
