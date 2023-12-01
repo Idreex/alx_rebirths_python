cff23441f05966525814ab88a4ca5a0b7faee290
Idreex <idreesademola07@gmail.com>
2023-12-01 23:29:11 +0100

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+
+class Square:
+    def __init__(self,size):
+        self.size = size
+
+    
+    @property
+    def size(self):
+        return self.__size
+        
+    @size.setter
+    def size(self, size):
+        self.__size = size
+
+        
+my_square = Square(3)
+print(type(my_square))
+print(my_square.__dict__)
+
+try:
+    print(my_square.size)
+except Exception as e:
+    print(e)
+
+try:
+    print(my_square.__size)
+except Exception as e:
+    print(e)
